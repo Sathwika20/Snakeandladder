ae65ad2c79f1fffb4bde8c3948df5407aa6b09c2
G.Sathwika <gopirajsathwika1999@gmail.com>
2021-02-23 15:11:07 +0530

    zero position

---
+++ SnakeandladderUC1.java
@@ -0,0 +1,10 @@
+public class SnakeandladderUC1 {
+
+   public static void main(String[] args) {
+
+  int POSITION = 0;
+
+  System.out.println("player position is " +POSITION);
+ }
+}
+
---
+++ SnakeandladderUC2.java
@@ -0,0 +1,8 @@
+public class SnakeandladderUC2 {
+  public static void main(String[] args) {
+
+ int dice = (int)(Math.random() * 10) % 6 + 1;
+  System.out.println("dice number is " +dice);
+  }
+}
+
